a58ca40d6ea402bff89d8c36d90bb322e739c968
Nikolay Vinge <vinge@megatec.ru>
2020-07-13 15:48:00 +0400

    web client vue js

---
+++ WebClientVueJs/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+    <div id="app">
+        <Auth />
+    </div>
+</template>
+
+<script>
+    import Auth from './components/Auth.vue';
+
+    export default {
+        name: 'app',
+        components: {
+            Auth
+        }
+    };
+</script>
+
+<style>
+</style>
---
+++ WebClientVueJs/src/components/Auth.vue
@@ -0,0 +1,48 @@
+<template>
+    <div class="auth">
+        <div>
+            <label>Login: </label>
+            <input type="text" v-model="login" />
+        </div>
+        <div>
+            <label>Password: </label>
+            <input type="text" v-model="password" />
+        </div>
+        <button v-on:click="send">Send</button>
+    </div>
+</template>
+
+<script>
+    import axios from 'axios';
+
+    export default {
+        name: 'Auth',
+        props: {
+        },
+        data() {
+            return {
+                login: "",
+                password: ""
+            };
+        },
+        mounted() {
+        },
+        methods: {
+            send: function () {
+                var postData = { login: this.login, password: this.password };
+                axios({
+                    method: 'post',
+                    url: 'https://localhost:44363/api/stub/login',
+                    data: postData
+                }).then(response => {
+                    if (response.data.success) {
+                        
+                    }
+                });
+            }
+        }
+    };
+</script>
+
+<style scoped>
+</style>
