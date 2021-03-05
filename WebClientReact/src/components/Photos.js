f229e444eca5f611b03d3a12bec73e1253021a31
Nikolay Vinge <vinge@megatec.ru>
2021-03-05 11:58:39 +0400

    React Окно подтверждения удаления фотки

--- WebClientReact/src/components/Photos.js
+++ WebClientReact/src/components/Photos.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Modal from 'react-modal';
 import { useHistory } from 'react-router-dom';
 import Axios from 'axios';
 import SignalR from '../api/signalr';
@@ -12,12 +13,37 @@ const styles = {
     }
 }
 
+const confirmDeletePhotoModalStyles = {
+    overlay: {
+        position: 'fixed',
+        top: 0,
+        left: 0,
+        right: 0,
+        bottom: 0,
+        backgroundColor: 'rgba(255, 255, 255, 0.8)'
+    },
+    content: {
+        position: 'absolute',
+        margin: 'auto',
+        width: '450px',
+        height: '120px',
+        border: '1px solid #ccc',
+        background: '#fff',
+        overflow: 'auto',
+        WebkitOverflowScrolling: 'touch',
+        borderRadius: '4px',
+        outline: 'none'
+    }
+}
+
 function Photos() {
-    const login = localStorage.getItem('auth-data-login');
-    const token = localStorage.getItem('auth-data-token');
-    const clientId = localStorage.getItem('auth-data-clientId');
-    const history = useHistory();
-    const [photos, setPhotos] = React.useState([]);
+    const login = localStorage.getItem('auth-data-login')
+    const token = localStorage.getItem('auth-data-token')
+    const clientId = localStorage.getItem('auth-data-clientId')
+    const history = useHistory()
+    const [photos, setPhotos] = React.useState([])
+    const [selectedDeletePhotoId, setSelectedDeletePhotoId] = React.useState()
+    const [isDeletePhotoConfirmOpen, setIsDeletePhotoConfirmOpen] = React.useState(false)
 
     function loadPhotosId() {
         Axios({
@@ -25,7 +51,7 @@ function Photos() {
             url: Config.getPhotosApiPath,
             data: { login: login, token: token, clientId: clientId }
         })
-    };
+    }
     
     function loadPhotosContent(photoIds) {
         for (let photoIdIndex in photoIds) {
@@ -34,16 +60,21 @@ function Photos() {
                 method: 'post',
                 url: Config.getPhotoApiPath,
                 data: { photoId: photoIds[photoIdIndex], login: login, token: token, clientId: clientId }
-            });
+            })
         }
-    };
+    }
 
-    function deletePhoto(photoId) {
+    function confirmDeletePhoto(photoId) {
+        setSelectedDeletePhotoId(photoId)
+        setIsDeletePhotoConfirmOpen(true)
+    }
+    
+    function deleteSelectedPhoto() {
         Axios({
             method: 'post',
             url: Config.deletePhotoApiPath,
-            data: { photoId: photoId, login: login, token: token, clientId: clientId }
-        });
+            data: { photoId: selectedDeletePhotoId, login: login, token: token, clientId: clientId }
+        })
     }
 
     function initSignalRResponses() {
@@ -54,7 +85,7 @@ function Photos() {
             } else {
                 loadPhotosContent(response.photoIds);
             }
-        });
+        })
         signalr.addHandler('GetPhotoResponse', function (response) {
             if (!response || !response.success) {
                 history.push('/');
@@ -62,25 +93,25 @@ function Photos() {
                 const photo = { id: response.photoId, image: 'data:image/png;base64,' + response.fileBase64Content };
                 setPhotos(photos => photos.concat(photo));
             }
-        });
+        })
         signalr.addHandler('UploadPhotosResponse', function (response) {
             if (!response || !response.success) {
                 history.push('/');
             } else {
                 loadPhotosContent([response.photoId]);
             }
-        });
+        })
         signalr.addHandler('DeletePhotoResponse', function (response) {
             if (!response || !response.success) {
                 history.push('/');
             } else {
                 setPhotos(photos => photos.filter(photo => photo.id !== response.photoId));
             }
-        });
+        })
         signalr.start(clientId).then(() => loadPhotosId());
     }
 
-    React.useEffect(() => { initSignalRResponses(); }, []);
+    React.useEffect(() => { initSignalRResponses(); }, [])
 
     return (
         <div>
@@ -92,12 +123,26 @@ function Photos() {
                         return  <li key={index} style={styles.li}>
                                     <div>
                                         <img src={photo.image} width='200' />
-                                        <a href='#' onClick={() => deletePhoto(photo.id)}><img src='/trash.png' /></a>
+                                        <a href='#' onClick={() => confirmDeletePhoto(photo.id)}><img src='/trash.png' /></a>
                                     </div>
                                 </li>
                     })}
                 </ul>
             :'нет фоток'}
+
+            <Modal appElement={document.getElementById('root')}
+                isOpen={isDeletePhotoConfirmOpen}
+                onRequestClose={() => setIsDeletePhotoConfirmOpen(false)}
+                shouldCloseOnOverlayClick={false}
+                style={confirmDeletePhotoModalStyles}>
+                <div>
+                    <p>Вы действительно хотите удалить выбранную фотографию?</p>
+                    <div style={{position: 'absolute', bottom: '16px', right: '16px'}}>
+                        <button onClick={() => { deleteSelectedPhoto(); setIsDeletePhotoConfirmOpen(false) }} style={{width: '75px', height: '24px'}}>Да</button>
+                        <button onClick={() => setIsDeletePhotoConfirmOpen(false)} style={{width: '75px', height: '24px', marginLeft: '8px'}}>Нет</button>
+                    </div>
+                </div>
+            </Modal>
         </div >
     )
 }
