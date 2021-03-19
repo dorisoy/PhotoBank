14467ac575ac437846b107b869a5940125803760
Nikolay Vinge <vinge@megatec.ru>
2021-03-19 09:16:02 +0400

    Стили для всплывающего сообщения

--- WebClientReact/src/components/Notification.js
+++ WebClientReact/src/components/Notification.js
@@ -2,20 +2,29 @@ import React from 'react'
 
 const styles = {
     root: {
-        background: 'red',
         float: 'right',
         width: '250px',
-        height: '60px'
+        height: '60px',
+        background: 'rgb(156 222 105)',
+        borderRadius: '6px',
+        boxShadow: '0 0 10px rgb(0 0 0 / 50%)'
+    },
+    text: {
+        display: 'table',
+        margin: 'auto',
+        verticalAlign: 'middle',
+        lineHeight: '50px',
+        textAlign: 'center'
     }
 }
 
 function Notification({isShow, onComplete, message}) {
     const [opacity, setOpacity] = React.useState(1.0)
-    //isShow = true
+
     React.useEffect(() => {
         if (isShow) {
             if (opacity > 0.0) {
-                setTimeout(() => { setOpacity(opacity => opacity > 0.01 ? opacity - 0.01 : 0.0) }, 10)
+                setTimeout(() => { setOpacity(opacity => opacity > 0.01 ? opacity - 0.01 : 0.0) }, 25)
             } else {
                 onComplete()
             }
@@ -27,7 +36,7 @@ function Notification({isShow, onComplete, message}) {
     return (
         <div style={{ opacity: opacity, display: isShow && opacity > 0.0 ? 'block' : 'none' }}>
             <div style={styles.root}>
-                <p>{message}</p>
+                <span style={styles.text}>{message}</span>
             </div>
         </div>
     )
